8add69d3bf4b982e868ed4736318777366677834
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-09-11 17:46:42 -0400

    started on routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,9 @@
 let express = require("express");
 let burger = require("../burger.js");
+
+module.exports = app => {
+
+    // Sets the get for the api/friends route
+    app.get("/", (req, res) => res.render("index",  ));
+      
+}
