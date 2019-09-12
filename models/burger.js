ec233839592b47eeccba76d81768f00befc0d623
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-09-12 16:13:11 -0400

    trying to figure out the pattern app not working

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,5 @@
 let orm = require("../config/orm");
 
-  $("#createburger").on("submit", event => {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
-
-    // [name=plan] will find an element with a "name" attribute equal to the string "plan"
-    let newBurger = {
-      burger: $("#createburger").val()
-    };
-
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
 
 //   $(".devour").on("submit", event => {
 //     // Make sure to preventDefault on a submit event.
